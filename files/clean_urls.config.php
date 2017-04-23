558d3cf53a16eec51fd7b227fc7179991fdde3c7
Imran Shahid <narmi79@gmail.com>
2017-04-23 22:27:03 +1000

    adding unused config file

---
+++ files/clean_urls.config.php
@@ -0,0 +1,7 @@
+<?php
+## https://doc.owncloud.org/server/9.1/admin_manual/configuration_server/index_php_less_urls.html
+
+$CONFIG = array(
+    'overwrite.cli.url' => 'http://localhost:4000',
+    'htaccess.RewriteBase' => '/',
+);
